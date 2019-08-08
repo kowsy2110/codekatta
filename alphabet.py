5b0c909b4e727a02d935a295bba13cbcd71a1bd9
kowsalya <53849166+kowsy2110@users.noreply.github.com>
2019-08-08 14:16:21 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,5 @@
 ch = input("Enter a character: ")
-if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
+if((ch>='e' and ch<= 'i') or (ch>='E' and ch<='I'))
     print(ch, "is an Alphabet")
 else:
     print(ch, "is not an Alphabet")
